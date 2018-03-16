80cc5b03fcdb629e179c88ad68538e9e5f419ef8
Denis Atkesone <denis.atkesone@gmail.com>
2018-03-16 10:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 
 const returnFirstTwoDrivers = function (arr) {
-  return arr.slice(0, 2)
+  return arr.slice(2)
 }
 
 const returnLastTwoDrivers = function (arr) {
-  return arr.slice(0, -2)
+  return arr.slice(-2)
 }
